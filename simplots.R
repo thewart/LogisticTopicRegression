17aba740178c36597a1386df5da239d94b3b7535
thewart <seth.madlonkay@gmail.com>
2017-06-17 02:24:37 -0400

    visualize simulation results

---
+++ simplots.R
@@ -0,0 +1,8 @@
+K = c(5,10,20,40,80) #from .txt file
+r2dat <- data.table(r2=scan("~/analysis/logtopreg/simulation/topiccor_Kseq.csv"),
+                    K=ordered(rep(K,K)),type="State content")
+
+n <- 200 #from .txt file
+r2dat <- rbind(r2dat,data.table(r2=scan("~/analysis/logtopreg/simulation/pr2_Kseq.csv"),
+                   K=rep(K,each=n) %>% ordered,type="Individual phenotypes"))
+ggplot(r2dat,aes(y=r2,x=K)) + geom_boxplot() + facet_grid( ~ type,scale="free") + ylab("Correlation")
